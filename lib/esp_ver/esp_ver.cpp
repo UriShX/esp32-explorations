591abd913da9f11ddeea6f165e2ec83c35c9c9f5
Uri Shani <36714978+UriShX@users.noreply.github.com>
2023-07-18 11:17:56 +0300

    get sha256 from fw (not helpful)

--- lib/esp_ver/esp_ver.cpp
+++ lib/esp_ver/esp_ver.cpp
@@ -2,6 +2,10 @@
 
 #include <esp_app_format.h>
 
+char _version[32];
+
+// extern "C"
+// {
 #include "esp_ota_ops.h"
 #include "esp_image_format.h"
 
@@ -9,3 +13,14 @@ String _esp_return_current_fw_version()
 {
     return String(esp_ota_get_app_description()->version);
 }
+
+void _esp_get_current_sha256(uint8_t * hash_ptr)
+{
+    for (int i = 0; i < 32; i++)
+        hash_ptr[i] = (esp_ota_get_app_description()->app_elf_sha256[i]);
+}
+
+// void _esp_get_ota_stream_fw_version(String * ota_fw_verwion)
+// {
+    
+// }
--- lib/esp_ver/esp_ver.h
+++ lib/esp_ver/esp_ver.h
@@ -4,5 +4,6 @@
 #include <Arduino.h>
 
 String _esp_return_current_fw_version();
+void _esp_get_current_sha256(uint8_t * hash_ptr);
 
 #endif
